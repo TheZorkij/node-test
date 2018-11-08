efafd0ade5477194bbffd4f545c5b96c52b9482a
thezorkij <misha_apr@list.ru>
2018-11-08 17:56:23 +0300

    3 tests

--- index.test.js
+++ index.test.js
@@ -3,3 +3,11 @@ const expression = require('./index');
 test ('1', ()=> {
     expect(expression(2,2,3)).toBe(7);
 });
+
+test ('2', ()=> {
+    expect(expression(1,5,2)).toBe(10);
+});
+
+test ('3', ()=> {
+    expect(expression(3,3,0)).toBe(27);
+});
